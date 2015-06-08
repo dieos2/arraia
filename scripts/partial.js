4da734b56d253b052319f27a01ddb0eef3a1a902
dieos2 <dieos2@gmail.com>
2015-06-08 15:22:08 -0300

    Houver menu
    agora com equio

--- scripts/partial.js
+++ scripts/partial.js
@@ -26,3 +26,25 @@ $.get("preencher.html", function (data) {
     $("#scripts").html(data);
 
 });
+setTimeout(function () {
+   
+
+var url = window.location.href;
+var menu = url.substring(url.lastIndexOf('/') + 1);
+debugger;
+if (menu == "index.html") {
+    $("#index").parent().addClass("selected")
+}
+if (menu == "agenda.html") {
+    $("#agenda").parent().addClass("selected")
+}
+if (menu == "galerias.html") {
+    $("#galerias").parent().addClass("selected")
+}
+if (menu == "videos.html") {
+    $("#videos").parent().addClass("selected")
+}
+if (menu == "noticias.html") {
+    $("#noticias").parent().addClass("selected")
+}
+}, 1000);
